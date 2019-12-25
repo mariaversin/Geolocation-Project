34c826e526cb80b987f4a27095a2b1d6b85f571f
maria <verdejosinmaria@gmail.com>
2019-12-25 13:08:03 +0100

    filter companies

---
+++ constants.py
@@ -0,0 +1,21 @@
+places = {
+    'airport': {
+        'color': 'red',
+        'icon': 'plane',
+        'rank': 20,
+        'distance': 20000,
+        'api': 'airport' },
+    'school': {
+        'color': 'yellow',
+        'icon': 'pen',
+        'rank': 10,
+        'distance': 1500,
+        'api': 'school'},
+    'party': {
+        'color': 'green',
+        'icon': 'glass',
+        'rank': 5,
+        'distance': 1000,
+        'api': 'party'
+    }
+}
---
+++ mongo.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+
+def connectCollection(database, collection):
+    ''' return collection from db crunchbase'''
+    db = client[database]
+    coll = db[collection]
+    return db, coll
