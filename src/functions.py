8b8aa1cf61f5f7f4d2e0f3fa268ea689b44b1b82
maria <verdejosinmaria@gmail.com>
2019-12-26 11:48:25 +0100

    cleaning

---
+++ src/functions.py
@@ -0,0 +1,63 @@
+from mongo import connectCollection
+import pandas as pd
+import numpy as np
+
+
+db, coll = connectCollection('dbcompanies','companies') 
+filter1 = [
+    {"$match": {"$and":
+            [{"founded_year": {"$gte": 2009}}, # Fundadas a partir del 2009
+             {"deadpooled_year": None}, # Que no hayan quebrado
+             {"offices.latitude": {"$exists": True, "$ne": None}}, # Que tengan latitud
+             {"offices.longitude": {"$exists": True, "$ne": None}}]}}] # Que tengan longitud
+
+offices = list(coll.aggregate(filter1))
+def prepareData(bd):
+    '''Function to clean data'''
+    cleanedItems = []
+    errors = 0
+    for group in offices:
+        for item in range(len(group['offices'])):
+            try:
+                cleanedItems.append({
+                "name":group["name"],
+                "employees": group['number_of_employees'],
+                "year":group['founded_year'],
+                "category":group['category_code'],
+                "id":group['_id'],
+                "money raised":group['total_money_raised'],
+                "city" : group['offices'][item]['city'],
+                "country" : group['offices'][item]['country_code'],
+                "latitude" : group['offices'][item]['latitude'],
+                "longitude" : group['offices'][item]['longitude'],
+                "coordinates":{
+                    "type":"Point",
+                    "coordinates":[group['offices'][item]['longitude'],group['offices'][item]['latitude']]}})
+            except Exception:
+                errors += 1
+                if errors > 0:
+                    print(f"Hay {errors} errores")
+    return cleanedItems
+
+data = pd.DataFrame(prepareData(offices))
+
+def money_clean(data):
+    
+    ''' Function to convert money raised to int '''
+    
+    data['money raised']= data['money raised'].replace('[Kk]', '*100',regex = True).replace( 'M', '*1000000',regex=True)
+    data['money raised']= data['money raised'].replace('[€$£]','',regex=True)
+    #data['money raised']= data['money raised'].apply(np.int64)
+    data['money raised']= data['money raised'].map(pd.eval)
+    #data=data[data!=0].dropna()
+    return money_comp
+
+def category(data):
+    
+    ''' Function to clean category column '''
+    
+    web_design = {'search': 'web','mobile': 'web','web': 'web','games_video': 'web','ecommerce': 'web','advertising': 'web',
+              'hardware': 'web','enterprise': 'web','network_hosting': 'web','software': 'web',
+              'analytics': 'web','cleantech': 'web'}
+    data = money_clean(data).replace(web_design, regex=True)
+    return money_comp
--- src/mongo.py
+++ src/mongo.py
@@ -7,3 +7,4 @@ def connectCollection(database, collection):
     db = client[database]
     coll = db[collection]
     return db, coll
+
